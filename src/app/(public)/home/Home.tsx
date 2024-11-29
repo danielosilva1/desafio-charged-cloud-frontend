020fbc77740f6be719c2328f427e1bb25db50b56
dosilva <danielosilva@alu.ufc.br>
2024-11-29 13:35:35 -0300

    implementa tela inicial

---
+++ src/app/(public)/home/Home.tsx
@@ -0,0 +1,78 @@
+import './home.css';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Card from 'react-bootstrap/Card';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+function Home() {
+    return (
+        <Container fluid='true' className='vh-100 d-flex flex-column'>
+            {/* Linha do Header */}
+            <Row className='w-100 text-center m-0'>
+                <Col sm>
+                    <Navbar className='bg-body-tertiary'>
+                        <Container>
+                            <Navbar.Brand href='/'>Desafio Charged</Navbar.Brand>
+                            <Navbar.Toggle />
+                            <Navbar.Collapse className='justify-content-end'>
+                                <Navbar.Text>
+                                    <a href='/login'>Entrar</a>
+                                </Navbar.Text>
+                            </Navbar.Collapse>
+                        </Container>
+                    </Navbar>
+                </Col>
+            </Row>
+            <Row className='flex-grow-1 d-flex justify-content-center text-center align-items-center'>
+                {/* Linhas para os cards */}
+                <Col sm={3} className='d-flex justify-content-center'>
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Img variant='top' src='../../../public/create.jpg' />
+                        <Card.Body>
+                            <Card.Title>Cadastre uma empresa</Card.Title>
+                            <Card.Text>
+                                Cadastre novas empresas e registre informações como CNPJ, nome, telefone e endereço.
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col sm={3} className='d-flex justify-content-center'>
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Img variant='top' src='../../../public/read.jpg' />
+                        <Card.Body>
+                            <Card.Title>Consulte empresas</Card.Title>
+                            <Card.Text>
+                                Veja os dados de todas as empresas ou filtre por correspondência parcial pelo CNPJ ou nome.
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col sm={3} className='d-flex justify-content-center'>
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Img variant='top' src='../../../public/update.jpg' />
+                        <Card.Body>
+                            <Card.Title>Atualize uma empresa</Card.Title>
+                            <Card.Text>
+                                Atualize os dados de uma empresa e mantenha o cadastro sempre atualizado.
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col sm={3} className='d-flex justify-content-center'>
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Img variant='top' src='../../../public/delete.jpg' />
+                        <Card.Body>
+                            <Card.Title>Exclua uma empresa</Card.Title>
+                            <Card.Text>
+                                Exclua os dados de uma empresa e mantenha a organização e integridade do banco de dados.
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+            <p>Imagens obtidas no Freepik</p>
+        </Container>
+    );
+}
+export default Home;
